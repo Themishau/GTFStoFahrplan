0498003a845fa6835a8e35475d833f02e56715bf
Themishau <40619652+Themishau@users.noreply.github.com>
2020-12-25 20:36:40 +0100

    d

--- main.py
+++ main.py
@@ -7,35 +7,45 @@ import pandas as pd
 from pandasql import sqldf
 from collections import defaultdict
 import tkinter as tk
-
+from tkinter import filedialog
 
 class Navbar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+    def __init__(self, root):
+        self.navbarFrame = tk.Frame(root)
+        self.navbarFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
 class Toolbar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+    def __init__(self, root):
+        self.toolbarFrame = tk.Frame(root)
+        self.menubar = tk.Menu(self.toolbarFrame)
+        self.filemenu = tk.Menu(self.menubar)
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+        self.toolbarFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+
 
 class Statusbar(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
+    def __init__(self, root):
+        self.statusbarFrame = tk.Frame(root)
+        self.statusbarFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-class Main(tk.Frame):
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.quitButton = tk.Button(self.parent, text = 'Quit', width = 25, command = self.close_windows)
-        self.quitButton.pack()
-        self.parent.pack()
+class SidePanel():
+    def __init__(self, root):
+        self.sidepanelFrame = tk.Frame(root)
+        self.sidepanelFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.sidepanelButton = tk.Button(self.sidepanelFrame, text="SidePanel ")
+        self.sidepanelButton.pack(side="top", fill=tk.BOTH)
+        self.clearButton = tk.Button(self.sidepanelFrame, text="Clear")
+        self.clearButton.pack(side="top", fill=tk.BOTH)
 
-    def close_windows(self):
-        self.parent.destroy()
+class Main():
+    def __init__(self, root):
+        self.mainFrame = tk.Frame(root)
+        self.mainFrame.pack(side=tk.RIGHT, fill=tk.BOTH, expand=1)
+        self.quitButton = tk.Button(self.mainFrame, text = "Quit")
+        self.quitButton.pack(side="top", fill=tk.BOTH)
+
 
 class Model():
-
     def __init__(self):
         self.xpoint = 200
         self.ypoint = 200
@@ -43,32 +53,57 @@ class Model():
 
 class Controller():
     def __init__(self):
-        self.root = tk.frame()
+        self.root = tk.Tk()
+        self.root.geometry("250x150+300+300")
         self.model = Model()
         self.view = View(self.root)
-        self.view.sidepanel.plotBut.bind("<Button>", self.my_plot)
-        self.view.sidepanel.clearButton.bind("<Button>", self.clear)
+        #bind side panel functions
+        self.view.sidePanel.sidepanelButton.bind("<Button>", self.my_plot)
+        self.view.sidePanel.clearButton.bind("<Button>", self.clear)
+        #bind main menu button
+        self.view.main.quitButton.bind("<Button>", self.close_program)
+        #bind tool bar functions
+        self.view.toolbar.filemenu.add_command(label="Exit", command=self.close_program_menu)
+        self.root.config(menu=self.view.toolbar.menubar)
 
     def run(self):
-        self.root.title("Tkinter MVC example")
+        self.root.title("GTFS to Fahrplan")
         self.root.deiconify()
         self.root.mainloop()
 
     def clear(self, event):
-        self.view.ax0.clear()
-        self.view.fig.canvas.draw()
+        print('clear')
+
+    def my_plot(self, event):
+        print('my_plot')
+
+    def OpenFile(self):
+        name = tk.filedialog()
+        print (name)
+
+    def About(self):
+        print ("This is a simple example of a menu")
+
+    def close_program(self, event):
+        self.root.destroy()
+
+    def close_program_menu(self):
+        self.root.destroy()
 
 class View():
-    def __init__(self, master):
-        self.frame = Tk.Frame(master)
-        self.fig = Figure(figsize=(7.5, 4), dpi=80)
-        self.ax0 = self.fig.add_axes(
-            (0.05, .05, .90, .90), facecolor=(.75, .75, .75), frameon=False)
-        self.frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=1)
-        self.sidepanel = SidePanel(master)
-        self.canvas = FigureCanvasTkAgg(self.fig, master=self.frame)
-        self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
-        self.canvas.draw()
+    def __init__(self, parent):
+        self.frame = tk.Frame(parent)
+        self.frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
+        self.sidePanel = SidePanel(parent)
+        self.statusbar = Statusbar(parent)
+        self.toolbar = Toolbar(parent)
+        self.navbar = Navbar(parent)
+        self.main = Main(parent)
+
+        #self.statusbar.pack(side="bottom", fill="x")
+        #self.toolbar.pack(side="top", fill="x")
+        #self.navbar.pack(side="left", fill="y")
+        #self.main.pack(side="right", fill="both", expand=True)
 
 class MainGUI(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -82,6 +117,7 @@ class MainGUI(tk.Frame):
         self.toolbar.pack(side="top", fill="x")
         self.navbar.pack(side="left", fill="y")
         self.main.pack(side="right", fill="both", expand=True)
+
 
         def close_windows(self):
             self.parent.destroy()
@@ -348,8 +384,8 @@ if __name__ == '__main__':
     print("start")
     #main()
     gtfsMenu = Controller()
+    gtfsMenu.run()
 
-    root.mainloop()
 
 # these are some attempts to get the data fast
 def maybefasterGetGTFS(tripsList, stopsList, stopTimesList, calendarList, calendar_datesList):
