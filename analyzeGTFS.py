8ad5fc1451e05753302f6145dbb0a112dc5fc96b
Themishau <40619652+Themishau@users.noreply.github.com>
2020-12-31 23:46:01 +0100

    d

--- analyzeGTFS.py
+++ analyzeGTFS.py
@@ -21,8 +21,11 @@ async def read_gtfs_data ():
         calendar_datesList = calendar_dates.readlines()
     with open("GTFS_VBB_bereichsscharf/routes.txt", "r", encoding="utf8") as routes:
         routesList = routes.readlines()
+    with open("GTFS_VBB_bereichsscharf/agency.txt", "r", encoding="utf8") as routes:
+        agencyList = routes.readlines()
 
-    gtfsData = [stopsList, stopTimesList, tripsList, calendarList, calendar_datesList, routesList]
+
+    gtfsData = [stopsList, stopTimesList, tripsList, calendarList, calendar_datesList, routesList, agencyList]
 
     stopsList = None
     stopTimesList = None
@@ -30,6 +33,7 @@ async def read_gtfs_data ():
     calendarList = None
     calendar_datesList = None
     routesList = None
+    agencyList = None
     return gtfsData
 
 async def get_gtfs_stop(inputgtfsData):
@@ -173,6 +177,28 @@ async def get_gtfs_routes(inputgtfsData):
     print("get_gtfs_routes loaded")
     return routesFahrtdict
 
+async def get_gtfs_agencies(inputgtfsData):
+    print("get_gtfs_agencies start")
+    agencyFahrtdict = []
+    for agency in inputgtfsData:
+        agency = agency.replace(", ", " ")
+        agency = agency.replace('"', "")
+        agency = agency.replace('\n', "")
+        agencyData = agency.split(",")
+        dataDict = {
+            "agency_id": agencyData[0],
+            "agency_name": agencyData[1],
+            "agency_url": agencyData[2],
+            "agency_timezone": agencyData[3],
+            "agency_lang": agencyData[4],
+            "agency_phone": agencyData[5],
+
+        }
+        agencyFahrtdict.append(dataDict)
+    routesList = None
+    print("get_gtfs_agencies loaded")
+    return agencyFahrtdict
+
 async def get_gtfs(inputgtfsData):
     """ Creating and starting 10 tasks.
     tasks = get_gtfs_stop(inputgtfsData[0]),\
@@ -189,140 +215,46 @@ async def get_gtfs(inputgtfsData):
     calendarWeekdict = await get_gtfs_calendarWeek(inputgtfsData[3])
     calendarDatesdict = await get_gtfs_calendarDates(inputgtfsData[4])
     routesFahrtdict = await get_gtfs_routes(inputgtfsData[5])
+    agencyFahrtdict = await get_gtfs_agencies(inputgtfsData[6])
 
-    return stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict
+    return stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict, agencyFahrtdict
     #await get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict)
     #await get_fahrt_ofroute_fahrplan(routeName2, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict)
 
-async def get_gtf_sdict(inputgtfsData):
-    tripdict = []
-    stopsdict = []
-    stopTimesdict = []
+def select_gtfs_routes_from_agancy(agency, routesFahrtdict):
+    inputVar = [{'agency_id': agency[0]}]
+    varTest = pd.DataFrame(inputVar).set_index('agency_id')
+    dfRoutes = pd.DataFrame(routesFahrtdict).set_index('route_id')
+    cond_routes_of_agency = '''
+                select dfRoutes.route_short_name
+                from dfRoutes 
+                left join varTest
+                where varTest.agency_id = dfRoutes.agency_id
+                order by dfRoutes.route_short_name;
+               '''
+    routes_list = sqldf(cond_routes_of_agency, locals())
+    routes_list.values.tolist()
+    #print (agency_list.values.tolist())
+    return routes_list.values.tolist()
 
-    calendarWeekdict = []
-    calendarDatesdict = []
-    routesFahrtdict = []
+async def read_gtfs_agencies(agencies_dict):
+    df_agency = pd.DataFrame(agencies_dict).set_index('agency_id')
+    cond_agencies = '''
+                select df_agency.agency_id, df_agency.agency_name
+                from df_agency 
+                order by df_agency.agency_id;
+               '''
+    agency_list = sqldf(cond_agencies, locals())
+    agency_list.values.tolist()
+    #print (agency_list.values.tolist())
+    return agency_list.values.tolist()
 
-    for haltestellen in inputgtfsData[0]:
-        haltestellen = haltestellen.replace(", ", " ")
-        haltestellen = haltestellen.replace('"', "")
-        haltestellen = haltestellen.replace('\n', "")
-        stopData = haltestellen.split(",")
-        dataDict = {
-            "stop_id": stopData[0],
-            "stop_code": stopData[1],
-            "stop_name": stopData[2],
-            "stop_desc": stopData[3],
-            "stop_lat": stopData[4],
-            "stop_lon": stopData[5],
-            "location_type": stopData[6],
-            "parent_station": stopData[7],
-            "wheelchair_accessible": stopData[8],
-            "platform_code": stopData[9],
-            "zone_id": stopData[10]
-        }
-        stopsdict.append(dataDict)
-    stopsList = None
-
-    for stopTime in inputgtfsData[1]:
-        stopTime = stopTime.replace(", ", " ")
-        stopTime = stopTime.replace('"', "")
-        stopTime = stopTime.replace('\n', "")
-        stopTimeData = stopTime.split(",")
-        dataDict = {
-            "trip_id": stopTimeData[0],
-            "arrival_time": stopTimeData[1],
-            "departure_time": stopTimeData[2],
-            "stop_id": stopTimeData[3],
-            "stop_sequence": stopTimeData[4],
-            "pickup_type": stopTimeData[5],
-            "drop_off_type": stopTimeData[6],
-            "stop_headsign": stopTimeData[7],
-
-        }
-        stopTimesdict.append(dataDict)
-    stopTimesList = None
-
-    for trip in inputgtfsData[2]:
-        trip = trip.replace(", ", " ")
-        trip = trip.replace('"', "")
-        trip = trip.replace('\n', "")
-        data = trip.split(",")
-        dataDict = {
-            "route_id": data[0],
-            "service_id": data[1],
-            "trip_id": data[2],
-            "trip_headsign": data[3],
-            "trip_short_name": data[4],
-            "direction_id": data[5],
-            "block_id": data[6],
-            "shape_id": data[7],
-            "wheelchair_accessible": data[8],
-            "bikes_allowed": data[9]
-        }
-        tripdict.append(dataDict)
-    tripsList = None
-
-    for calendarDate in inputgtfsData[3]:
-        calendarDate = calendarDate.replace(", ", " ")
-        calendarDate = calendarDate.replace('"', "")
-        calendarDate = calendarDate.replace('\n', "")
-        calendarData = calendarDate.split(",")
-        dataDict = {
-            "service_id": calendarData[0],
-            "monday": calendarData[1],
-            "tuesday": calendarData[2],
-            "wednesday": calendarData[3],
-            "thursday": calendarData[4],
-            "friday": calendarData[5],
-            "saturday": calendarData[6],
-            "sunday": calendarData[7],
-            "start_date": calendarData[8],
-            "end_date": calendarData[9]
-        }
-        calendarWeekdict.append(dataDict)
-    calendarList = None
-
-    for calendarDate in inputgtfsData[4]:
-        calendarDate = calendarDate.replace(", ", " ")
-        calendarDate = calendarDate.replace('"', "")
-        calendarDate = calendarDate.replace('\n', "")
-        calendarDatesData = calendarDate.split(",")
-        dataDict = {
-            "service_id": calendarDatesData[0],
-            "date": calendarDatesData[1],
-            "exception_type": calendarDatesData[2],
-        }
-        calendarDatesdict.append(dataDict)
-    calendar_datesList = None
-
-    for routes in inputgtfsData[5]:
-        routes = routes.replace(", ", " ")
-        routes = routes.replace('"', "")
-        routes = routes.replace('\n', "")
-        routesData = routes.split(",")
-        dataDict = {
-            "route_id": routesData[0],
-            "agency_id": routesData[1],
-            "route_short_name": routesData[2],
-            "route_long_name": routesData[3],
-            "route_type": routesData[4],
-            "route_color": routesData[5],
-            "route_text_color": routesData[6],
-            "route_desc": routesData[7]
-        }
-        routesFahrtdict.append(dataDict)
-    routesList = None
-
-    routeName = '100'
-    print("GTFS loaded")
-    #getFahrtOfroute_short_name(routeName, stopTimesdict, tripdict, stopsdict, routesFahrtdict)
-    get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict)
 
 # tried to get all data in one variable but then I need to create a new index for every dict again
 # maybe I try to get change it later
-async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict):
+async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict, agencyFahrtdict):
     print("get_fahrt_ofroute_fahrplan start")
+    print(routeName)
     last_time = time.time()
 
     #DataFrame for every route
@@ -343,6 +275,9 @@ async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdi
     #DataFrame with every service dates
     dfDates = pd.DataFrame(calendarDatesdict).set_index('service_id', 'date')
 
+    #DataFrame with every agency
+    df_agency = pd.DataFrame(agencyFahrtdict).set_index('agency_id')
+
     weekDay = [{'Monday': 'Monday'},
                {'Tuesday': 'Tuesday'},
                {'Wednesday': 'Wednesday'},
@@ -355,6 +290,7 @@ async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdi
     # dataframe with the (bus) lines
     inputVar = [{'route_short_name': routeName}]
     varTest = pd.DataFrame(inputVar).set_index('route_short_name')
+
 
     cond_weekdays= '''
                 select dfTrips.trip_id, dfTrips.route_id, dfStopTimes.stop_sequence, dfStops.stop_name, dfStopTimes.stop_headsign, dfStopTimes.arrival_time
@@ -372,8 +308,9 @@ async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdi
                 order by dfTrips.trip_id, dfStopTimes.arrival_time;
                '''
 
+    #select dfStops.stop_name, GROUP_CONCAT(dfStopTimes.arrival_time, ';') as 'stop times mon-fri'
     cond_Fahrplan= '''
-                select dfStops.stop_name, GROUP_CONCAT(dfStopTimes.arrival_time, ';') as 'stop times mon-fri'
+                select dfStops.stop_name, GROUP_CONCAT(Nullif(dfStopTimes.arrival_time,'-'), ';') as 'stop times mon-fri'
                 from dfStopTimes 
                 inner join dfTrips on dfStopTimes.trip_id = dfTrips.trip_id
                 inner join dfStops on dfStopTimes.stop_id = dfStops.stop_id
@@ -424,7 +361,7 @@ async def get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdi
     dfRoutes = None
     dfWeek = None
 
-    fahrplan.to_csv(routeName + '.csv', header=True, quotechar=' ', index='', sep=';', mode='w', encoding='utf8')
+    fahrplan.to_csv(routeName + '.csv', header=True, quotechar='-', index='', sep=';', mode='w', encoding='utf8')
 
     zeit = time.time() - last_time
     print("time: {} ".format(zeit))
--- gui.py
+++ gui.py
@@ -24,31 +24,45 @@ class Statusbar(tk.Frame):
         self.statusbarFrame = tk.Frame(root)
         self.statusbarFrame.pack(side=tk.BOTTOM, fill=tk.BOTH)
 
-class SidePanel():
+class SidePanel(tk.Frame):
     def __init__(self, root):
         self.sidepanelFrame = tk.Frame(root)
         self.sidepanelFrame.pack(side=tk.RIGHT, fill=tk.BOTH)
-        self.sidepanelButton = tk.Button(self.sidepanelFrame, text="Start")
-        self.sidepanelButton.pack(side="top", fill=tk.BOTH)
-        self.LoadGTFSButton = tk.Button(self.sidepanelFrame, text="Load/Check GTFS")
-        self.LoadGTFSButton.pack(side="top", fill=tk.BOTH)
+
         self.quitButton = tk.Button(self.sidepanelFrame, text="Quit")
         self.quitButton.pack(side="bottom", fill=tk.BOTH)
 
-class Main():
+        self.LoadGTFSButton = tk.Button(self.sidepanelFrame, text="Load/Check GTFS")
+        self.LoadGTFSButton.pack(side="bottom", fill=tk.BOTH)
+
+        self.sidepanelButton = tk.Button(self.sidepanelFrame, text="Start")
+        self.sidepanelButton.pack(side="bottom", fill=tk.BOTH)
+
+
+
+class Main(tk.Frame):
     def __init__(self, root):
         self.mainFrame = tk.Frame(root)
-        self.namelbl = tk.Label(self.mainFrame, text="Name")
-        self.namelbl.pack(side="left")
-        self.entryRoute = tk.Entry(self.mainFrame)
-        self.entryRoute.pack(side="right", fill=tk.BOTH)
+        self.namelbl = tk.Label(self.mainFrame, text="Agency and Routes")
+        self.namelbl.pack(side="top")
         self.mainFrame.pack(side=tk.TOP)
+
+        #lists of agency
+        self.agency_List = tk.Listbox(self.mainFrame)
+        self.agency_List_scrollbar = tk.Scrollbar(self.agency_List, orient="vertical")
+        self.agency_List.config(yscrollcommand=self.agency_List_scrollbar.set)
+        self.agency_List_scrollbar.config(command=self.agency_List.yview)
+        self.agency_List.pack(side=tk.LEFT)
+
+        #lists of routes
+        self.routes_List = tk.Listbox(self.mainFrame)
+        self.routes_List_scrollbar = tk.Scrollbar(self.routes_List, orient="vertical")
+        self.routes_List.config(yscrollcommand=self.routes_List_scrollbar.set)
+        self.routes_List_scrollbar.config(command=self.routes_List.yview)
+        self.routes_List.pack(side=tk.RIGHT)
 
 class Model():
     def __init__(self):
-        self.xpoint = 200
-        self.ypoint = 200
-        self.res = None
         self.GTFSData = None
         self.routeNames = '100'
         self.stopsdict = None
@@ -57,31 +71,53 @@ class Model():
         self.calendarWeekdict = None
         self.calendarDatesdict = None
         self.routesFahrtdict = None
+        self.agencyFahrtdict = None
+        self.agencies = None
+        self.routesList = None
+        self.selectedAgency = None
+        self.selectedRoute = None
 
+
+    def dataLoadedAndAvailable(self):
+        if (self.stopsdict == None or self.stopTimesdict== None or self.tripdict == None or self.calendarWeekdict == None or self.calendarDatesdict == None or self.routesFahrtdict == None):
+            return False
+        return True
+
+    #import the data into the data framework
     async def import_GTFS(self):
         await self.readGFTS()
         await self.getGTFS()
+        self.agencies = await read_gtfs_agencies(self.agencyFahrtdict)
         print("GTFS imported")
 
+    #reads the files
     async def readGFTS(self):
         self.GTFSData = await read_gtfs_data()
 
+    #gets the data out of the files
     async def getGTFS(self):
-        self.stopsdict, self.stopTimesdict, self.tripdict, self.calendarWeekdict, self.calendarDatesdict, self.routesFahrtdict = await get_gtfs(self.GTFSData)
+        self.stopsdict, self.stopTimesdict, self.tripdict, self.calendarWeekdict, self.calendarDatesdict, self.routesFahrtdict, self.agencyFahrtdict = await get_gtfs(self.GTFSData)
         self.GTFSData = None
 
+    def getRoutesOfAgency(self, agency):
+        self.routesList = select_gtfs_routes_from_agancy(agency, self.routesFahrtdict)
+        print("routes of agency loaded")
+
     async def createFahrplan(self):
-        if (self.stopsdict == None or self.stopTimesdict== None or self.tripdict == None or self.calendarWeekdict == None or self.calendarDatesdict == None or self.routesFahrtdict == None):
+        if (self.dataLoadedAndAvailable()):
+            if (self.selectedRoute != None):
+                routeName = [self.selectedRoute]
+                tasks = [get_fahrt_ofroute_fahrplan(name, self.stopsdict, self.stopTimesdict, self.tripdict, self.calendarWeekdict, self.calendarDatesdict, self.routesFahrtdict, self.agencyFahrtdict) for name in routeName]
+                completed, pending = await asyncio.wait(tasks)
+        else:
             messagebox.showerror( 'Fehler', 'Noch keine Daten')
             return
-        routeName = ['100','1']
-        tasks = [get_fahrt_ofroute_fahrplan(name, self.stopsdict, self.stopTimesdict, self.tripdict, self.calendarWeekdict, self.calendarDatesdict, self.routesFahrtdict) for name in routeName]
-        completed, pending = await asyncio.wait(tasks)
         #results = [task.result() for task in completed]
 
         #get_fahrt_ofroute_fahrplan(routeName, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict)
         #get_fahrt_ofroute_fahrplan(routeName2, stopsdict, stopTimesdict, tripdict, calendarWeekdict, calendarDatesdict, routesFahrtdict)
         #slow_get_gtf_sdict(GTFSData)
+
 
 
 class Controller():
@@ -91,12 +127,16 @@ class Controller():
         self.root = tk.Tk()
 
         #init window size
-        self.root.geometry("250x250+300+300")
+        self.root.geometry("500x300+400+400")
 
         self.runningAsync = 0
         #init model and viewer
         self.model = Model()
         self.view = View(self.root)
+
+
+        #bind main buttions and functions
+        self.view.main.agency_List.bind('<<ListboxSelect>>', self.selection_agency)
 
         #bind side panel buttons and functions
         self.view.sidePanel.sidepanelButton.bind("<Button>", self.start)
@@ -109,29 +149,83 @@ class Controller():
         self.root.config(menu=self.view.toolbar.menubar)
 
 
+
+    def selection_agency(self, event):
+        try:
+            selected_agency = None
+            for agency in self.model.agencies:
+                if (agency[1] == self.view.main.agency_List.selection_get()):
+                    print(self.view.main.agency_List.selection_get())
+                    selected_agency = agency
+            if (selected_agency == None):
+                print("error nothing found")
+                return
+            self.model.getRoutesOfAgency(selected_agency)
+            self.update_routes_List()
+        except:
+            messagebox.showerror('Error', 'Nothing Selected!')
+
+    def update_routes_List(self):
+        if (self.view.main.routes_List != None):
+            self.view.main.routes_List.delete(0,'end')
+        for routes in self.model.routesList:
+            self.view.main.routes_List.insert("end", routes[0])
+            #self.view.main.agency_List.grid(row=0, column=0, columnspan=1)
+        print("routes list updated")
+
+    def update_agency_List(self):
+        for agency in self.model.agencies:
+            #printAgency = agency[0] + ", " + agency[1]
+            self.view.main.agency_List.insert("end", agency[1])
+            #self.view.main.agency_List.grid(row=0, column=0, columnspan=1)
+        print("agency list updated")
+
     def async_loadGTFS(self):
+        #clear list
+        if (self.view.main.agency_List != None):
+            self.view.main.agency_List.delete(0,'end')
+            self.view.main.routes_List.delete(0,'end')
+
+        #check if program is already running
         if(self.runningAsync > 0):
-            messagebox.showerror( 'Error', 'Program is already running')
+            messagebox.showerror('Error', 'Program is already running')
             return
 
         loop = asyncio.new_event_loop()
         self.runningAsync = self.runningAsync + 1
         loop.run_until_complete(self.model.import_GTFS())
+        self.update_agency_List()
         loop.close()
 
         self.runningAsync = self.runningAsync - 1
+
+    def select_route(self):
+        try:
+            selected_route = None
+            selected_route = self.view.main.routes_List.selection_get()
+            if (selected_route == None):
+                print("error no route selected")
+                return False
+            self.model.selectedRoute = selected_route
+            return True
+        except:
+            messagebox.showerror('Error', 'Something went wrong!')
 
     def async_create_Fahrplan(self):
         if(self.runningAsync > 0):
             messagebox.showerror( 'Error', 'Program is already running')
             return
 
-        loop = asyncio.new_event_loop()
-        self.runningAsync = self.runningAsync + 1
-        loop.run_until_complete(self.model.createFahrplan())
-        loop.close()
+        #checks and sets the selected route for fahrplan
+        if (self.select_route()):
+            loop = asyncio.new_event_loop()
+            self.runningAsync = self.runningAsync + 1
+            loop.run_until_complete(self.model.createFahrplan())
+            loop.close()
+            self.runningAsync = self.runningAsync - 1
+        else:
+            messagebox.showerror( 'Error', 'no route selected')
 
-        self.runningAsync = self.runningAsync - 1
 
     def do_tasks(self, button):
         """ Function/Button starting the asyncio part. """
@@ -150,6 +244,7 @@ class Controller():
         button = "loadGTFS"
         self.do_tasks(button)
 
+
     def start(self, event):
         button = "loadFahrplan"
         self.do_tasks(button)
@@ -157,7 +252,6 @@ class Controller():
     def analyze_gtfs(self):
         button = "loadFahrplan"
         self.do_tasks(button)
-
 
     def open_file(self):
         name = tk.filedialog()
@@ -177,12 +271,13 @@ class Controller():
 class View():
     def __init__(self, parent):
         self.frame = tk.Frame(parent)
-        self.frame.grid(sticky="NSEW")
+        #self.frame.grid(sticky="NSEW")
         self.frame.pack(fill=tk.BOTH)
+        self.main = Main(parent)
         self.sidePanel = SidePanel(parent)
         self.statusbar = Statusbar(parent)
         self.toolbar = Toolbar(parent)
         self.navbar = Navbar(parent)
-        self.main = Main(parent)
+
 
 
