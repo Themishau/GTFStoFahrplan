e985acfaa116ef758cab42ae49ef55eaa9e3c76e
Themishau <40619652+Themishau@users.noreply.github.com>
2022-06-17 10:07:32 +0200

    delete old programs and added new gui elements

--- mainq5.py
+++ mainq5.py
@@ -3,7 +3,6 @@
 from GUI_Q5 import main
 
 if __name__ == '__main__':
-    print("start")
     main(['update_process',
           'toggle_button_direction_event',
           'toggle_button_date_week_event',
