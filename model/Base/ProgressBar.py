7716ad8c897f62071ea0d577a3ea4efd98b07196
Themishau <40619652+Themishau@users.noreply.github.com>
2024-07-21 20:50:20 +0200

    refactor(MVVM model): wip changing to DTOs

--- model/Base/ProgressBar.py
+++ model/Base/ProgressBar.py
@@ -1,17 +1,4 @@
 # -*- coding: utf-8 -*-
-from model.observer import Publisher, Subscriber
-import time
-import pandas as pd
-from pandasql import sqldf
-import zipfile
-import io
-from datetime import datetime, timedelta
-import re
-import logging
-import sys
-import os
-
-from PyQt5.QtCore import QAbstractTableModel
 
 class ProgressBar:
     def __init__(self):
